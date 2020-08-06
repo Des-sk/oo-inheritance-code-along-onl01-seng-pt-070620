4e7e55d12022e8b74b8a52fdc740900ddd047bf0
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-06 20:17:30 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -4,8 +4,19 @@ class Vehicle
  def initialize(wheel_size, wheel_number)
    @wheel_size = wheel_size
    @wheel_number = wheel_number
-end
+ end
 
-def global_variables
-  
+ def global_variables
+  "vrrrrrrrooom!"
+ end
+ 
+ 
+
+
+
+
+
+
+
+
 end
